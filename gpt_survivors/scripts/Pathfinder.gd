df05ee53a47787945633f6a16ae1e24329d6def3
bjc1269 <bcarpenter5@nait.ca>
2024-05-25 22:32:52 -0600

    added navagation agent

--- gpt_survivors/scripts/Pathfinder.gd
+++ gpt_survivors/scripts/Pathfinder.gd
@@ -4,13 +4,20 @@ extends Node2D
 @export var target : Node2D = null
 @export var Find_target_rate = 0.5
 
-@onready var pathfinding_atgent= $NavigationAgent2D
+var pathfinding_agent : NavigationAgent2D = null
+
 
 func _ready():
 
 	if target == null:
 		# find the player
 		target = get_node("/root/Player")
+	
+	#find the NavigationAgent2D child
+	pathfinding_agent = get_node("NavigationAgent2D")
+	if pathfinding_agent == null:
+		pathfinding_agent = NavigationAgent2D.new()
+		add_child(pathfinding_agent)
 
 
 
@@ -27,17 +34,17 @@ func _ready():
 
 
 func find_target():
-	pathfinding_atgent.target_position = target.global_position
+	pathfinding_agent.target_position = target.global_position
 
 
 # return the next position to move to or null if the navigation is finished
 func next_position():
-	if pathfinding_atgent.is_navigation_finished():
+	if pathfinding_agent.is_navigation_finished():
 		return null
 
 
 
-	return pathfinding_atgent.get_next_path_position()
+	return pathfinding_agent.get_next_path_position()
 
 
 
