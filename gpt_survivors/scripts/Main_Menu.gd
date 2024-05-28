f7c01edbfcb47c099534754f62f14fd1647b1479
Alex Downey <adowney5@studentmail.nait.ca>
2024-05-27 18:04:04 -0600

    Main Menu Ready!
    Full implementation of main menu, need options still.

---
+++ gpt_survivors/scripts/Main_Menu.gd
@@ -0,0 +1,31 @@
+class_name MainMenu
+extends Control
+
+@onready var start_button = $MarginContainer/ButtonsHBoxContainer/ButtonsVBoxContainer/Start_Button as Button
+@onready var options_button = $MarginContainer/ButtonsHBoxContainer/ButtonsVBoxContainer/Options_Button as Button
+@onready var exit_button = $MarginContainer/ButtonsHBoxContainer/ButtonsVBoxContainer/Exit_Button as Button
+@onready var start_level = preload("res://scenes/Test_Scenes/Brandon/ricky_test.tscn") as PackedScene
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	start_button.button_up.connect(on_start_released)
+	options_button.button_up.connect(on_options_released)
+	exit_button.button_up.connect(on_exit_released)
+	
+	pass # Replace with function body.
+
+func on_start_released() -> void:
+	get_tree().change_scene_to_packed(start_level)
+
+
+func on_options_released() -> void:
+	pass
+	
+func on_exit_released() -> void:
+	get_tree().quit()
+
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
